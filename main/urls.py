180db15a48ce43b3c2674e58552ed782d280c680
RizvanuSN <rizvanusn@gmail.com>
2023-11-15 16:18:19 +0700

    selesai tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import register
 from main.views import login_user 
@@ -7,6 +7,9 @@ from main.views import edit_product
 from main.views import get_product_json
 from main.views import add_product_ajax
 from main.views import delete_product
+from main.views import create_product_flutter
+from main.views import logout
+
 
 app_name = 'main'
 
@@ -23,5 +26,10 @@ urlpatterns = [
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
-     path('delete/<int:id>', delete_product, name='delete_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),
+    path('auth/', include('authentication.urls')),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
+    path('logout/', logout, name='logout'),
+
+
 ]
--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -32,10 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'main',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,3 +138,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
